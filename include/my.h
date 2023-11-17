70ea74c06022abfe91c6096a3ebac63faed4fd87
Lauric Dulery <lauric.dulery@epitech.eu>
2023-11-17 15:54:07 +0100

    version coding style semi fini pour le main mais trkl, toujours le bug quand laisser appuyé

--- include/my.h
+++ include/my.h
@@ -6,22 +6,28 @@
 */
 
 #ifndef MY_H_
-      #define MY_H_
+    #define MY_H_
 
-      #include <SFML/Audio.h>
-      #include <stdbool.h>
-      #include <SFML/Graphics.h>
-      #include <stdlib.h>
-      #include <SFML/Window.h>
-      #include <SFML/System.h>
-      #include <SFML/Graphics/Rect.h>
-      #include <SFML/System/Clock.h>
-      #include "struct.h"
-      #include <stdio.h>
+    #include <SFML/Audio.h>
+    #include <stdbool.h>
+    #include <SFML/Graphics.h>
+    #include <stdlib.h>
+    #include <SFML/Window.h>
+    #include <SFML/System.h>
+    #include <SFML/Graphics/Rect.h>
+    #include <SFML/System/Clock.h>
+    #include "struct.h"
+    #include <stdio.h>
 
-      #define sfCf sfTexture_createFromFile
-      #define sfWc sfRenderWindow_create
+    #define sftexture_createfromfile
+    #define sfrenderwindow_create
 
-      void game_function(sfRenderWindow* window, sfEvent event);
-
+void game_function(sfRenderWindow* window, sfEvent event);
+void handle_events(sfRenderWindow* window, sfEvent event, sfMusic* music);
+sfRenderWindow* init_window(void);
+sfMusic* init_music(void);
+sfSprite* init_sprite(const char *filePath, sfVector2f scale);
+sfSprite* init_button(void);
+sfSprite* init_title(void);
+void handle_click(sfRenderWindow* window, sfEvent event, sfMusic* music);
 #endif
--- src/my_main.c
+++ src/my_main.c
@@ -8,87 +8,116 @@
 #include <unistd.h>
 #include "../include/my.h"
 
-//Fonction click
-void if_click(sfRenderWindow* window, sfEvent event, sfMusic* music)
+int main(void)
+{
+    sfRenderWindow* window = init_window();
+    sfEvent event;
+    sfMusic* music = init_music();
+    sfSprite* sprite = init_sprite("content/game.png", (sfVector2f){1.3, 1.3});
+    sfSprite* button = init_button();
+    sfSprite* title = init_title();
+
+    while (sfRenderWindow_isOpen(window)) {
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            my_while(window, event);
+            handle_events(window, event, music);
+        }
+        handle_events(window, event, music);
+        sfRenderWindow_clear(window, sfWhite);
+        sfRenderWindow_drawSprite(window, sprite, NULL);
+        sfRenderWindow_drawSprite(window, title, NULL);
+        sfRenderWindow_drawSprite(window, button, NULL);
+        sfRenderWindow_display(window);
+        handle_click(window, event, music);
+    }
+    sfRenderWindow_destroy(window);
+    sfMusic_destroy(music);
+    sfSprite_destroy(sprite);
+    sfSprite_destroy(button);
+    sfSprite_destroy(title);
+    return 0;
+}
+
+void my_while(sfRenderWindow* window, sfEvent event)
+{
+    if (event.type == sfEvtClosed) {
+        sfRenderWindow_close(window);
+    }
+}
+
+void handle_events(sfRenderWindow* window, sfEvent event, sfMusic* music)
+{
+    while (sfRenderWindow_pollEvent(window, &event)) {
+        if (event.type == sfEvtClosed) {
+            sfRenderWindow_close(window);
+        }
+    }
+}
+
+sfRenderWindow* init_window(void)
+{
+    sfVideoMode mode = {1280, 720, 32};
+
+    return sfRenderWindow_create(mode, "My Hunter", sfTitlebar |
+        sfClose, NULL);
+}
+
+sfMusic* init_music(void)
+{
+    sfMusic* music = sfMusic_createFromFile("content/mainsongluigi.ogg");
+
+    if (!music) {
+        exit(EXIT_FAILURE);
+    }
+    sfMusic_setLoop(music, true);
+    sfMusic_play(music);
+    return music;
+}
+
+sfSprite* init_sprite(const char *filePath, sfVector2f scale)
+{
+    sfTexture* texture = sfTexture_createFromFile(filePath, NULL);
+    sfSprite* sprite = sfSprite_create();
+
+    sfSprite_setTexture(sprite, texture, sfTrue);
+    sfSprite_setScale(sprite, scale);
+    return sprite;
+}
+
+sfSprite* init_button(void)
+{
+    sfSprite* button = init_sprite("content/startbutton.png", (sfVector2f){12, 12});
+    sfVector2f posButton = {225, 50};
+
+    sfSprite_setPosition(button, posButton);
+    return button;
+}
+
+sfSprite* init_title(void)
+{
+    sfSprite* title = init_sprite("content/Zombie.png", (sfVector2f){9, 9});
+    sfVector2f posTitle = {400, 6};
+
+    sfSprite_setPosition(title, posTitle);
+    return title;
+}
+
+void handle_click(sfRenderWindow* window, sfEvent event, sfMusic* music)
 {
     sfVector2i mouse = sfMouse_getPosition(window);
 
     if (mouse.x >= 471 && mouse.x <= 807) {
         if (mouse.y >= 486 && mouse.y <= 581) {
-            if (event.type == sfEvtMouseButtonPressed) {
-                sfMusic_stop(music);
-                sfMusic_destroy(music);
-                game_function(window, event);
-            }
+            suite_click(window, event, music);
         }
     }
 }
 
-int main(void)
+void suite_click(sfRenderWindow* window, sfEvent event, sfMusic* music)
 {
-    sfVideoMode mode = {1280, 720, 32};
-    sfRenderWindow* window;
-    sfEvent event;
-
-    //Music
-    sfMusic* music;
-    music = sfMusic_createFromFile("content/mainsongluigi.ogg");
-    if (!music)
-    {
-        return EXIT_FAILURE;
+    if (event.type == sfEvtMouseButtonPressed) {
+        sfMusic_stop(music);
+        sfMusic_destroy(music);
+        game_function(window, event);
     }
-    sfMusic_setLoop(music, true);
-    sfMusic_play(music);
-            
-    //Def sprites
-    sfTexture *texture = sfTexture_createFromFile("content/game.png", NULL);
-    sfSprite *sprite = sfSprite_create();
-
-    sfTexture *texture02 = sfTexture_createFromFile("content/startbutton.png", NULL);
-    sfSprite *button = sfSprite_create();
-
-    sfTexture *titre = sfTexture_createFromFile("content/Zombie.png", NULL);
-    sfSprite *sprite_titre = sfSprite_create();
-
-    //Taille sprite
-    sfVector2f scale = {1.3, 1.3};
-    sfVector2f scale_button = {12, 12};
-    sfVector2f scale_titre = {9, 9};
-
-    //Position sprite
-    sfVector2f pos_button = {225, 50};
-    sfVector2f pos_titre = {400, 6};
-
-    //Affichage image de fond
-    sfSprite_setTexture(sprite, texture, sfTrue);
-    sfSprite_setScale(sprite, scale);
-
-    //Affichage bouton
-    sfSprite_setTexture(button, texture02, sfTrue);
-    sfSprite_setScale(button, scale_button);
-    sfSprite_setPosition(button, pos_button);
-
-    //Affichage Titre
-    sfSprite_setTexture(sprite_titre, titre, sfTrue);
-    sfSprite_setScale(sprite_titre, scale_titre);
-    sfSprite_setPosition(sprite_titre, pos_titre);
-
-    window = sfRenderWindow_create(mode, "My Hunter", sfTitlebar | sfClose, NULL);
-
-    //Ouverture page avec sprites
-    while (sfRenderWindow_isOpen(window)) {
-        while (sfRenderWindow_pollEvent(window, &event)) {
-            if (event.type == sfEvtClosed) {
-                sfRenderWindow_close(window);
-            }
-        }
-        sfRenderWindow_clear(window, sfWhite);
-        sfRenderWindow_drawSprite(window, sprite, NULL);
-        sfRenderWindow_drawSprite(window, sprite_titre, NULL);
-        sfRenderWindow_drawSprite(window, button, NULL);
-        sfRenderWindow_display(window);
-
-        if_click(window, event, music);
-    }
-    return 0;
 }
